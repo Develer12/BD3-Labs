601a708391088f13a7196dd1016659b2dbcf9705
Develer12 <cenia-v@mail.ru>
2020-04-11 13:06:14 +0300

    Update user creating

--- Lab6/oralce.sql
+++ Lab6/oralce.sql
@@ -2,8 +2,18 @@
 --1. https://sourceforge.net/projects/jtds/?source=typ_redirect
 
 --2.
-create user migrant identified by migrant;
-grant all PRIVILEGES to migrant;
+alter session set "_ORACLE_SCRIPT"=true;
+create user Migrant identified by Migrant default tablespace Users temporary tablespace temp;
+
+
+grant all privileges to Migrant WITH ADMIN OPTION;
+GRANT CONNECT, RESOURCE, CREATE VIEW, CREATE PUBLIC SYNONYM TO 
+  Migrant WITH ADMIN OPTION;
+  
+  
+create user WHiring identified by WHiring default tablespace Users temporary tablespace temp;
+grant all privileges to WHiring WITH ADMIN OPTION;
+
 
 --3. connect to that user
 
